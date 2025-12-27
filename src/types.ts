12b47dfb9c14462c92c64840aaa1f050afe18f34
saitogo555 <sg.saitogo@gmail.com>
2025-12-27 11:23:34 +0900

    feat: ロゴ生成機能と型定義を追加

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface LogoOptions {
+  size?: number;
+  text: string;
+  textColor?: string | string[];
+  fontSource?: string;
+  fontFamily?: string;
+  fontSize?: number;
+  fontWeight?: string | number;
+  backgroundColor?: string | string[];
+}
