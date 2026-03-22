1b79999eef2c9e377b1b4d0fd30982935be49ef4
saitogo555 <sg.saitogo@gmail.com>
2026-03-22 20:35:32 +0900

    fix(constants): フォントソースとフォントウェイトを修正

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_SIZE = 100;
 export const DEFAULT_TEXT_COLOR = "#ffffff";
 export const DEFAULT_FONT_SOURCE =
-	"https://cdn.jsdelivr.net/fontsource/fonts/raleway@latest/latin-900-normal.woff2";
-export const DEFAULT_FONT_WEIGHT = 900;
+	"https://cdn.jsdelivr.net/fontsource/fonts/raleway@latest/latin-700-normal.woff2";
+export const DEFAULT_FONT_WEIGHT = 700;
 export const DEFAULT_BG_COLOR = "#000000";
